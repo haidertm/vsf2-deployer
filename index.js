aac805eab3c6f88ee1202bf9c3995eb0c3850f2b
Syed Haider Hassan <syed.haider@ki5.co.uk>
2023-09-22 21:28:31 +0500

    InProgress: working on artifacts download

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
   console.log(testMsg);
   res.send(testMsg);
 });
-app.post('/deploy', (req, res) => {
+app.post('/deploy', async (req, res) => {
   const { user, repo, trigger, branch, commit } = req.body;
 
   console.log(`Trigger: ${trigger}`);
@@ -25,16 +25,57 @@ app.post('/deploy', (req, res) => {
   console.log(`Commit: ${commit}`);
 
   const token = process.env.GITHUB_TOKEN;
-  const githubRepo = 'tilemountainuk/vsf2-nuxt3'; // Replace with your GitHub username and repo name
 
-  exec('./deploy.sh', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing script: ${error}`);
-      return;
+  try {
+    const { data } = await axios.get(`https://api.github.com/repos/${repo}/actions/artifacts`, {
+      headers: {
+        Authorization: `token ${token}`,
+      },
+    });
+
+    console.log('artifacts', data, data?.artifacts);
+
+    const artifact = data.artifacts.find(artifact => artifact.name === 'YOUR_ARTIFACT_NAME'); // Replace with your artifact name
+
+    if (artifact) {
+      const downloadUrl = artifact.archive_download_url;
+
+      const { data: fileData } = await axios.get(downloadUrl, {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+        responseType: 'arraybuffer',
+      });
+
+      const filePath = path.join(__dirname, 'artifact.zip');
+      fs.writeFileSync(filePath, fileData);
+
+      // Proceed with your deployment script
+      exec('./deploy.sh', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing script: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
     }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+
+  } catch (error) {
+    console.error(`Failed to download artifact: ${error}`);
+    res.status(500).send(error);
+  }
+
+  // const githubRepo = 'tilemountainuk/vsf2-nuxt3'; // Replace with your GitHub username and repo name
+
+  // exec('./deploy.sh', (error, stdout, stderr) => {
+  //   if (error) {
+  //     console.error(`Error executing script: ${error}`);
+  //     return;
+  //   }
+  //   console.log(`stdout: ${stdout}`);
+  //   console.error(`stderr: ${stderr}`);
+  // });
 
   // res.status(200).send('Received');
   res.status(200).send(req.body);
