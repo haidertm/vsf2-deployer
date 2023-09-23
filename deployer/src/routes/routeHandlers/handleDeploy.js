2f8f5094d63be16ea8a70d94345e87071317e511
Syed Haider Hassan <syed.haider@ki5.co.uk>
2023-09-23 15:54:31 +0500

    In Progress the new Architecture for Continous Deployment.

--- deployer/src/routes/routeHandlers/handleDeploy.js
+++ deployer/src/routes/routeHandlers/handleDeploy.js
@@ -2,18 +2,22 @@ import deployerAction from '../../services/deployer.js';
 import { config } from '../../config/index.js'
 
 export default async (req, res) => {
-
-  const { user, repo, trigger, branch, commit, token } = req.body;
-
-  console.log(`Trigger: ${ trigger }`);
-  console.log(`repo: ${ repo }`);
-  console.log(`Branch: ${ branch }`);
-  console.log(`Commit: ${ commit }`);
-
-  const result = await deployerAction({ token, commitHash: commit });
-  if (result.status && result.message) {
-    res.status(result.status).send(result.message);
-  } else {
-    res.send(result);
+  try {
+    const { user, repo, trigger, branch, commit, token } = req.body;
+    console.log(`Trigger: ${ trigger }`);
+    console.log(`repo: ${ repo }`);
+    console.log(`Branch: ${ branch }`);
+    console.log(`Commit: ${ commit }`);
+    res.status(200).send(req.body);
+  } catch (err) {
+    res.status(500).send(err);
   }
+
+
+  // const result = await deployerAction({ token, commitHash: commit });
+  // if (result.status && result.message) {
+  //   res.status(result.status).send(result.message);
+  // } else {
+  //   res.send(result);
+  // }
 };
