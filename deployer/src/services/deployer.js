cb6b2585eecff29a40fe9eec830f02e604dda751
Syed Haider Hassan <syed.haider@ki5.co.uk>
2023-09-23 23:43:55 +0500

    In Progress the new Architecture for Continous Deployment.

--- deployer/src/services/deployer.js
+++ deployer/src/services/deployer.js
@@ -15,22 +15,27 @@ export default async ({ token, commitHash }) => {
   const { status, data } = await githubApi.getArtifacts({ token, repo: config.repo });
 
   if (!data) {
-    return {
+
+    const message = {
       error: true,
       status,
       message: 'Something went wrong with data, We dont have data in response'
     };
+    console.log(message)
+    return message;
   }
 
   const { artifacts, total_count } = data;
 
   if (total_count === 0 || artifacts.length === 0) {
-    // res.status(404).send('No artifacts were found to deploy');
-    return {
+    const message = {
       error: true,
       status: 404,
       message: 'No artifacts were found to deploy'
     };
+    // res.status(404).send('No artifacts were found to deploy');
+    console.log({ message });
+    return message;
   }
 
   console.log('Handling Deployment');
@@ -41,12 +46,16 @@ export default async ({ token, commitHash }) => {
   const artifact = artifacts.find(artifact => artifact?.workflow_run?.head_sha && artifact.workflow_run.head_sha === commitHash); // Replace with your artifact name
 
   if (!artifact) {
-    return {
+    const message = {
       error: true,
       status: 404,
       message: `Artifact was not found for workflow commit ${ commitHash }`
     };
+    console.log({ message })
+    return message;
   }
+
+  console.log('We have the artifact, Handling the ArtifactNow');
 
   return handleArtifacts({ artifact, token, commitHash })
 };
