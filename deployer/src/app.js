a1ef84bc782ab3e5fd348170f4476d851bd4563d
Syed Haider Hassan <syed.haider@ki5.co.uk>
2023-09-23 15:39:56 +0500

    In Progress the new Architecture for Continous Deployment.

--- deployer/src/app.js
+++ deployer/src/app.js
@@ -6,6 +6,13 @@ import { config } from './config/index.js';
 const app = express();
 
 app.use(bodyParser.json());
+
+//Using a middleware to identify issue.
+app.use((req, res, next) => {
+  console.log('Incoming Request Body:', req.body);
+  next();
+});
+
 app.use('/', routes);
 
 const port = config.port;
